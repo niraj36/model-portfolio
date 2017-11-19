7fd7543cb6f16d112c8d313f74bb4184cbe6a167
niraj36 <niraj36@dmazurevm1.(none)>
2017-11-19 02:01:06 +0000

    Adding database model.

---
+++ data_access/database/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+Base = declarative_base()
+
+class Investment(Base):
+    __tablename__ = 'Investment'
+    Id = Column(Integer, primary_key=True)
+    Ticker = Column(String, nullable=False)
+    Description = Column(String, nullable=False)
+
+    def __repr__(self):
+        return 'Id: {}, Ticker {}, Description {}'.format(self.Id, self.Ticker, self.Description)
+
+class MarketDataType(Base):
+    Id = Column(Integer, primary_key=True)
+    Description = Column(String, nullable=False)
+
+    def __repr__(self):
+        return 'Id {}, Description {}'.format(self.Id, self.Description)
+
+
+class MarketData (Base):
+    __tablename__ = 'MarketData'
+    Id = Column(Integer, primary_key=True)
+    InvestmentId = Column(Integer, ForeignKey(Investment.Id), nullable=False)
+    MarketDataTypeId = Column(Integer, ForeignKey(MarketData.Id), nullable=False)
+    Value = Column(float, nullable=False)
+    Source = Column(String, nullable=False)
+    CreateDate = Column(DateTime, default=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return 'Id {}, InvestmentId {}, MarketDataTypeId {}, Value {}, Source {}, CreateDate{}'\
+            .format(self.Id, self.InvestmentId, self.MarketDataTypeId, self.Value, self.Source, self.CreateDate)
+    
